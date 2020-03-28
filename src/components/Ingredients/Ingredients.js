6088e04331632bd37f45ac49233011455b77fccb
Samuel Idehen <samuelidehenlion@gmail.com>
2020-03-28 14:04:04 -0400

    --UseEfect set up

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,11 +7,42 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch("https://burger-reactapp-bf88e.firebaseio.com/Items.json")
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        const IngredientData = [];
+        for (const key in responseData) {
+          IngredientData.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(IngredientData);
+      });
+  }, []);
+
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ]);
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://burger-reactapp-bf88e.firebaseio.com/Items.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-type": "application.json" }
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      });
   };
 
   const removeIngredientHandler = id => {
